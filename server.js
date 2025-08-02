873ea075003f988f3259d70c6bb5bb5837f9ee1e
Isaac Alexander <Isaacalexander81@gmail.com>
2025-08-02 02:49:12 +0100

    used postman

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 import http from 'http';
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html')
-    res.statusCode = 404;
-    res.end('<h1>Hello World!</h1>');
+   if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>HomePage</h1>');
+   } else if (req.url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>About</h1>');
+   } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('<h1>Not Found</h1>');
+   }
+
 });
 
 server.listen(PORT, () => {
-    console.log((`Server running on port ${PORT}`)); 
+    console.log((`Server running on port ${PORT}`));
 });
